322a82f64ed8cbd1d312dd950e16b6c3e9f9eec5
Noman Rafiq <heyimnomee@gmail.com>
2024-01-23 12:11:40 +0500

    filteration and sorting is in place

--- controllers/products.js
+++ controllers/products.js
@@ -2,11 +2,34 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
     const products = await Product.find({});
-    return res.status(200).json({ products });
+    return res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'Products Actual route' });
+    const { featured, rating, name, price, company, sort } = req.query;
+    const queryObject = {};
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    if (company) {
+        //we can pass the regex search option from mongo to look for a small part in name
+        queryObject.company = { $regex: company, $options: 'i' };
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' };
+    }
+    let result = Product.find(queryObject); /// we find the products
+
+    //Sorting doesn't affect the amount of items we return
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList); //we sort the products if the sort is passed
+    } else {
+        result = result.sort('name'); //or it will sort by default by name a-z
+    }
+    const products = await result; // we wait for finding and sorting
+    console.log('queryObject:', queryObject);
+    res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
